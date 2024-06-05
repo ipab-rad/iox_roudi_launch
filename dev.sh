2a46ea938ea59a154cef74b1095f528184c7376a
hect95 <hector_cruz95@live.com>
2024-06-05 18:14:54 +0100

    Remove SCRIPT_DIR abs path when mounting volumes

--- dev.sh
+++ dev.sh
@@ -9,13 +9,10 @@ DOCKER_BUILDKIT=1 docker build \
     -t av_iox_roudi_launch:latest-dev \
     -f Dockerfile --target dev .
 
-# Get the absolute path of the script
-SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
-
 # Run docker image with local code volumes for development
 docker run -it --rm --net host --privileged \
     -v /dev:/dev \
     -v /tmp:/tmp \
-    -v $SCRIPT_DIR/roudi_config.toml:/opt/ros_ws/roudi_config.toml \
+    -v ./roudi_config.toml:/opt/ros_ws/roudi_config.toml \
     -v /etc/localtime:/etc/localtime:ro \
     av_iox_roudi_launch:latest-dev
